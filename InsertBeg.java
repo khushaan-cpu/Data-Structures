4891803851e9de3bafc29b722ae591fc69e122ff
Khushaan Garg <68324650+khushaan-cpu@users.noreply.github.com>
2020-07-27 00:05:53 +0530

    Create InsertBeg.java

---
+++ InsertBeg.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class InsertAfter
+{
+    Node head
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+    public void insert(int d)
+    {
+        Node newNode = new Node(d);
+        if(head == null)
+        {
+            head = newNode;
+            return;
+        }
+        Node last = head;
+        while(last.next!=null)
+        {
+            last = last.next;
+        }
+        last.next = newNode;
+        return;
+    }
+    public void printList
+    {
+        Node n = head;
+        while(n!=null)
+        {
+            System.out.println(n.data+" ");
+            n = n.next;
+        }
+        return;
+    }
+    public static void main(String[] args) {
+        InsertAfter list = new InsertAfter();
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            list.insert(obj.nextInt());
+        }
+    }
+}
